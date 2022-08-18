537c16eeb78f8196a7d3d6b64b578cb467342fe7
Interested-Guy <1001.tejas@gmail.com>
2022-08-18 17:26:42 +0530

    sfdgsdfg

---
+++ app/src/main/java/com/example/sensor/MainActivity.java
@@ -0,0 +1,167 @@
+package com.example.sensor;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorManager;
+import android.hardware.SensorEventListener;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * SensorListeners demonstrates how to gain access to sensors (here, the light
+ * and proximity sensors), how to register sensor listeners, and how to
+ * handle sensor events.
+ */
+public class MainActivity extends AppCompatActivity
+        implements SensorEventListener {
+
+    // System sensor manager instance.
+    private SensorManager mSensorManager;
+
+    // Proximity and light sensors, as retrieved from the sensor manager.
+    private Sensor mSensorProximity;
+    private Sensor mSensorLight;
+
+    // TextViews to display current sensor values.
+    private TextView mTextSensorLight;
+    private TextView mTextSensorProximity;
+    private Socket client;
+    private PrintWriter printwriter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize all view variables.
+        mTextSensorLight = (TextView) findViewById(R.id.label_light);
+        mTextSensorProximity = (TextView) findViewById(R.id.label_proximity);
+
+        // Get an instance of the sensor manager.
+        mSensorManager = (SensorManager) getSystemService(
+                Context.SENSOR_SERVICE);
+
+        // Get light and proximity sensors from the sensor manager.
+        // The getDefaultSensor() method returns null if the sensor
+        // is not available on the device.
+        mSensorProximity = mSensorManager.getDefaultSensor(
+                Sensor.TYPE_PROXIMITY);
+        mSensorLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+        // Get the error message from string resources.
+        String sensor_error = getResources().getString(R.string.error_no_sensor);
+
+        // If either mSensorLight or mSensorProximity are null, those sensors
+        // are not available in the device.  Set the text to the error message
+        if (mSensorLight == null) { mTextSensorLight.setText(sensor_error); }
+        if (mSensorProximity == null) {
+            mTextSensorProximity.setText(sensor_error);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Listeners for the sensors are registered in this callback and
+        // can be unregistered in onPause().
+        //
+        // Check to ensure sensors are available before registering listeners.
+        // Both listeners are registered with a "normal" amount of delay
+        // (SENSOR_DELAY_NORMAL)
+        if (mSensorProximity != null) {
+            mSensorManager.registerListener(this, mSensorProximity,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (mSensorLight != null) {
+            mSensorManager.registerListener(this, mSensorLight,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // Unregister all sensor listeners in this callback so they don't
+        // continue to use resources when the app is paused.
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+        // The sensor type (as defined in the Sensor class).
+        int sensorType = sensorEvent.sensor.getType();
+
+        // The new data value of the sensor.  Both the light and proximity
+        // sensors report one value at a time, which is always the first
+        // element in the values array.
+        float currentValue = sensorEvent.values[0];
+
+        switch (sensorType) {
+            // Event came from the light sensor.
+            case Sensor.TYPE_LIGHT:
+                // Set the light sensor text view to the light sensor string
+                // from the resources, with the placeholder filled in.
+                mTextSensorLight.setText(getResources().getString(
+                        R.string.label_light, currentValue));
+                new Thread(new ClientThread(getResources().getString(
+                        R.string.label_light, currentValue))).start();
+                break;
+            case Sensor.TYPE_PROXIMITY:
+                // Set the proximity sensor text view to the light sensor
+                // string from the resources, with the placeholder filled in.
+                mTextSensorProximity.setText(getResources().getString(
+                        R.string.label_proximity, currentValue));
+                break;
+            default:
+                // do nothing
+        }
+    }
+
+    /**
+     * Abstract method in SensorEventListener.  It must be implemented, but is
+     * unused in this app.
+     */
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+    }
+    class ClientThread implements Runnable {
+        private final String message;
+
+        ClientThread(String message) {
+            this.message = message;
+        }
+        @Override
+        public void run() {
+            try {
+                // the IP and port should be correct to have a connection established
+                // Creates a stream socket and connects it to the specified port number on the named host.
+                client = new Socket("192.168.0.202", 4444); // connect to server
+                printwriter = new PrintWriter(client.getOutputStream(),true);
+                printwriter.write(message); // write the message to output stream
+                printwriter.flush();
+                printwriter.close();
+                // closing the connection
+                client.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // updating the UI
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+
+                }
+            });
+        }
+    }
+}
